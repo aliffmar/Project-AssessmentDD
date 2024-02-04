7d54a6d5072a319835ee345508d60287667ae8b5
Aliff Ruslan <nav_spect@yahoo.com>
2024-02-04 13:43:28 +0000

    moving folders

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
+
 const express = require('express');
+const cors = require('cors');
+const { connectToMongoDB } = require('./db');
+const { ObjectId } = require("mongodb");
 
 const app = express();
-//app.use(json());
-//app.use(cors());
-
-
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+// Use CORS middleware
+app.use(cors());
+
+async function main() {
+  try {
+    const db = await connectToMongoDB();
+    console.log('Connected to MongoDB');
+
+    app.get('/', (req, res) => {
+      res.send('Hello, world!');
+      // You can use the `db` object here to interact with MongoDB
+    });
+
+    // app.post('/books', async (req, res) => {
+    //     try {
+    //       const { title, genre, authors, year, rating } = req.body;
+  
+    //       // Validation
+    //       if (!title || !genre || !authors || !year || !rating) {
+    //         return res.status(400).json({ message: 'Missing required fields' });
+    //       }
+  
+    //       const newBook = { title, genre, authors, year, rating };
+    //       const result = await booksCollection.insertOne(newBook);
+  
+    //       res.status(201).json(result.ops[0]);
+    //     } catch (error) {
+    //       res.status(500).json({ message: 'Error adding new book', error: error.message });
+    //     }
+    //   });
+  
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+  }
+}
+
+main();
